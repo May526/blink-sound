a62fbf46ab9f1789db2e808402249cb93c6b367c
May526 <dohatamayu@douhatamayunoMacBook-Pro.local>
2023-07-20 22:00:47 +0900

    first commit

--- ozyamamushi/__init__.py
+++ ozyamamushi/__init__.py
@@ -1,8 +1,8 @@
 import os
-
+import sys
 
 def get_package_root() -> str:
-    return os.path.dirname(os.path.abspath(__file__))
+    return os.path.dirname(os.path.abspath(sys.argv[0]))
 
 
 def get_pj_root() -> str:
